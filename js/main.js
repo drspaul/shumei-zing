c0148964956ce4f9be214d7c4ebb14a09c2c46b6
drspaul <63319088+drspaul@users.noreply.github.com>
2020-07-16 12:00:52 -0700

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,25 +1,17 @@
-$(document).ready(function($){
-   $(".navbar-toggle").click(function(e){
-      e.preventDefault();
-      $(".navbar-toggle").toggleClass("active");
-      $(".main-nav").toggleClass("active");
-    })
-});
-
-
-
-//menu toggle
+// --------------------------------------------------
+// Navigation Toggle
+// --------------------------------------------------
 (function($){
-    $('.nav-toggle').click(function(e){
-        e.preventDefault();
-        $('.nav-toggle').toggleClass('active');
-        $('.nav-menu').toggleClass('active');
-        $('.nav-overlay').toggleClass('active');
-    })
-    $('.nav-overlay').click(function(e){
-        e.preventDefault();
-        $('.nav-toggle').toggleClass('active');
-        $('.nav-menu').toggleClass('active');
-        $('.nav-overlay').toggleClass('active');
-    })
+  $('.nav-toggle').click(function(e){
+    e.preventDefault();
+    $('.nav-toggle').toggleClass('active');
+    $('.nav-menu').toggleClass('active');
+    $('.nav-overlay').toggleClass('active');
+  })
+  $('.nav-overlay').click(function(e){
+    e.preventDefault();
+    $('.nav-toggle').toggleClass('active');
+    $('.nav-menu').toggleClass('active');
+    $('.nav-overlay').toggleClass('active');
+  })
 })(jQuery);
