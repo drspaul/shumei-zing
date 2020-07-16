04a39c1416a0eed0771bea0dc54a3a631323d8d9
drspaul <63319088+drspaul@users.noreply.github.com>
2020-07-15 23:59:07 -0700

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,12 +1,25 @@
-window.onscroll = function() {myFunction()};
+$(document).ready(function($){
+   $(".navbar-toggle").click(function(e){
+      e.preventDefault();
+      $(".navbar-toggle").toggleClass("active");
+      $(".main-nav").toggleClass("active");
+    })
+});
 
-var navbar = document.getElementById("navBar");
-var sticky = navbar.offsetTop;
 
-function myFunction() {
-  if (window.pageYOffset >= sticky) {
-    navbar.classList.add("sticky")
-  } else {
-    navbar.classList.remove("sticky");
-  }
-}
+
+//menu toggle
+(function($){
+    $('.nav-toggle').click(function(e){
+        e.preventDefault();
+        $('.nav-toggle').toggleClass('active');
+        $('.nav-menu').toggleClass('active');
+        $('.nav-overlay').toggleClass('active');
+    })
+    $('.nav-overlay').click(function(e){
+        e.preventDefault();
+        $('.nav-toggle').toggleClass('active');
+        $('.nav-menu').toggleClass('active');
+        $('.nav-overlay').toggleClass('active');
+    })
+})(jQuery);
