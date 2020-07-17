ae6476c24108ea398a9fcb1642ff936cb27035b2
drspaul <63319088+drspaul@users.noreply.github.com>
2020-07-17 10:23:05 -0700

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -2,16 +2,21 @@
 // Navigation Toggle
 // --------------------------------------------------
 (function($){
-  $('.nav-toggle').click(function(e){
+  $(".nav-toggle").click(function(e){
     e.preventDefault();
-    $('.nav-toggle').toggleClass('active');
-    $('.nav-menu').toggleClass('active');
-    $('.nav-overlay').toggleClass('active');
+    $(".nav-toggle").toggleClass("active");
+    $(".nav-menu").toggleClass("active");
+    $(".nav-overlay").toggleClass("active");
   })
-  $('.nav-overlay').click(function(e){
+  $(".nav-overlay").click(function(e){
     e.preventDefault();
-    $('.nav-toggle').toggleClass('active');
-    $('.nav-menu').toggleClass('active');
-    $('.nav-overlay').toggleClass('active');
+    $(".nav-toggle").toggleClass("active");
+    $(".nav-menu").toggleClass("active");
+    $(".nav-overlay").toggleClass("active");
   })
+  $(".nav-menu li a").click(function() {
+    $(".nav-toggle").removeClass("active");
+    $(".nav-menu").removeClass("active");
+    $(".nav-overlay").removeClass("active");
+  });
 })(jQuery);
